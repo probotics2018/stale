189e89ffc702cb1954994406ae9994731276f393
rycao18 <rycao18@gmail.com>
2018-04-19 13:43:32 -0500

    change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = async robot => {
   robot.on('schedule.repository', markAndSweep)
 
   async function unmark (context) {
-    console.log("unmarking now")
+    console.log("please do something")
     if (!context.isBot) {
       const stale = await forRepository(context)
       let issue = context.payload.issue || context.payload.pull_request
